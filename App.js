f393f8d28c8e879cefbdc0fd13c8f1d1556db00c
matiaslang <matiaslang@icloud.com>
2020-01-22 11:25:28 +0200

    adding router

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Routes from './Routes.js'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Halojata halooo</Text>
+      <Routes />
     </View>
   )
 }
