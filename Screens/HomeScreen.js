d5c07f2fe1ff0102051e5e69970d30785138d45d
shonkam <samu.honkamaki@gmail.com>
2020-02-05 22:28:11 +0200

    homescreenia

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,71 +1,121 @@
-import React from 'react'
-import { Button, StyleSheet, Text, Image, View } from 'react-native'
+import React, { Component, Fragment } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Navigator,
+  Image
+} from 'react-native'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 
 class HomeScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
-    return {
-      headerTitle: 'HoMe' //() => <LogoTitle />,
-    }
+    return {}
   }
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <View>
-          <Button
-            title='Places nearby'
+      
+     
+     
+      
+        
+      
+      <View style={styles.container}>
+       <Image 
+       source={require('../assets/Logo/Logo_2-01.png')}
+       style={styles.image}
+       />
+      
+        <View style={styles.placesNearby}>
+          <TouchableOpacity
             onPress={() => this.props.navigation.navigate('Places')}
-          />
+          >
+            <Text style={styles.button}>Places nearby</Text>
+          </TouchableOpacity>
+          <Text style={styles.linePlaces}>________________________</Text>
+          <View />
         </View>
-        <View style={{ flexDirection: 'row' }}>
-          <Image
-            style={{ width: 150, height: 150 }}
-            source={require('../assets/Logo/Logo_2-01.png')}
-          />
-          <Image
-            style={{ width: 200, height: 200 }}
-            source={require('../assets/Logo/Logo_2-01.png')}
-          />
-        </View>
-        <Image
-          style={{ width: 250, height: 300 }}
-          source={require('../assets/Logo/Logo_2-01.png')}
-        />
-        <View>
-          <Button
-            title='History'
+
+        <View style={styles.history}>
+          <TouchableOpacity
             onPress={() => this.props.navigation.navigate('History')}
-          />
+          >
+            <Text style={styles.button}>History</Text>
+          </TouchableOpacity>
+          <Text style={styles.lineHistory}>________________________</Text>
         </View>
+
+        
       </View>
-    )
+
+      
+
+
+
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#d12a2a',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  placesNearby: {
-    flex: 1,
-    backgroundColor: '#bfcfff',
-    borderColor: '#043353',
-    borderRadius: 10,
-    paddingVertical: 50,
-    paddingHorizontal: 20
-  },
-  demo: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#ffffff',
+    alignItems: 'center'
+    
   },
 
-  header: {}
+  image:{
+        width: 150,
+        height: 150,
+        top: 20
+  },
+
+  linePlaces: {
+    color: '#043353',
+    textAlign: 'center',
+    paddingVertical: 275,
+    fontSize: 20
+  },
+
+  lineHistory: {
+    color: '#043353',
+    textAlign: 'center',
+    paddingVertical: 135,
+    fontSize: 20
+  },
+
+  button: {
+    backgroundColor: '#043353',
+    color: '#F7F7F7',
+    fontSize: 25,
+    alignSelf: 'stretch',
+    paddingVertical: 10,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    textAlign: 'center'
+  },
+
+  placesNearby: {
+    backgroundColor: '#F7F7F7',
+    borderColor: '#043353',
+    borderRadius: 30,
+    borderWidth: 3,
+    height: 400,
+    width: 350
+  },
+  history: {
+    backgroundColor: '#F7F7F7',
+    borderColor: '#043353',
+    borderRadius: 30,
+    borderWidth: 3,
+    height: 260,
+    width: 350,
+    fontSize: 25,
+    marginVertical: 15
+  }
 })
 
 export default HomeScreen
